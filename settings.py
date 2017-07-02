b068b3f90812fb9751c77626b78045d7ba7a884c
Jonathan <jonathan.spengeman@gmail.com>
2017-07-02 01:15:53 -0400

    Added Google drive integration.
    The results from the tests are now all being written to google
    drive instead of to a csv on disk.

---
+++ settings.py
@@ -0,0 +1,22 @@
+# OAuth2 related settings.
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'internet data speed test'
+
+# Google drive related settings.
+RANGE_NAME = 'A:F'
+SPEADSHEET_ID='1-17CPVQXD6WVcUCLzPTLcHlN6pkwMfhhsYAdjRgkLDo'
+VALUE_INPUT_OPTION='RAW'
+GOOGLE_DRIVE_URL='https://sheets.googleapis.com/$discovery/rest?version=v4'
+
+# Conversion constant for converting bits to mega bits.
+BITS_TO_MEGA_BITS = 1000000
+
+# Runtime options
+ACCESS_POINT = 'RT-AC1200'
+DEVICE = 'MBP'
+NUMBER_OF_TESTS = 5
+INTERNVAL = .25
+STRESSED_NETWORK = False
+ROOM = "bedroom"
+NOTES = ""
--- src.py
+++ src.py
@@ -1,57 +1,92 @@
+import os
+import httplib2
+from settings import *
 from pyspeedtest import SpeedTest
 from datetime import datetime, timedelta
+from apiclient import discovery
 from time import sleep
 from csv import writer
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
 
-# config: (device, notes)
-# device, ap, ping, ul, dl, notes
+def get_credentials():
+    """
+    Gets valid user credentials from storage.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+       'internet-speed-test-data.json')
 
-class Internet:
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
 
-    BITS_TO_MEGA_BITS = 1000000
 
-    def __init__(self, ap, device, duration, interval, notes=None):
-        self.ap = ap
-        self.device = device
-        self.notes = notes
-        self.test(duration, interval)
+def run_speed_test(ap, device, number_of_tests, interval, stressed, room, notes=""):
+    """
+    Run the speed test for a duration given some interval.
+    """
+    count = 0
+    st = SpeedTest()
+    test_data = []
 
-    def test(self, duration, interval):
-        """
-        Run the speed test for a duration given some interval.
-        """
-        count = 1
-        st = SpeedTest()
-        start_time = datetime.now()
-        time_delta = timedelta(minutes=duration)
-
-        while start_time + time_delta > datetime.now():
-            print "Running speed test: " + str(count)
-            self.write(
-                st.ping(),
-                st.download() / Internet.BITS_TO_MEGA_BITS,
-                st.upload() / Internet.BITS_TO_MEGA_BITS
-            )
-            count += 1
+    while number_of_tests > count:
+        count += 1
+        print "Running speed test: " + str(count)
+        try:
+            test_data.append([
+                ap,
+                device,
+                round(st.ping(), 2),
+                round(st.download() / BITS_TO_MEGA_BITS, 2),
+                round(st.upload() / BITS_TO_MEGA_BITS, 2),
+                stressed,
+                room,
+                notes
+            ])
             sleep(interval * 60)
+        except:
+            print "There was an exception while testing."
 
-        print "Completed %d speed tests" % count
-
-    def write(self, ping, dl, ul):
-        """
-        Write the test results to csv.
-        """
-        with open('output.csv', 'a') as output:
-            csv_writer = writer(output)
-            csv_writer.writerow(
-                [self.device, self.ap, ping, dl, ul, self.notes]
-            )
+    if len(test_data) > 0:
+        append_to_drive(test_data)
+    print "Completed %d speed tests" % count
 
 
-Internet(
-    ap="RT-AC1200",
-    device="MBP",
-    duration=60,
-    interval=1,
-    notes="Test was conducted in close proximity to the router"
+def append_to_drive(values):
+    """
+    Append the data products to the google spreadsheet.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build(
+        'sheets',
+        'v4',
+        http=http,
+        discoveryServiceUrl=GOOGLE_DRIVE_URL
+    )
+    service.spreadsheets().values().append(
+        spreadsheetId=SPEADSHEET_ID,
+        range=RANGE_NAME,
+        valueInputOption=VALUE_INPUT_OPTION,
+        body={'values': values}
+    ).execute()
+
+run_speed_test(
+    ap=ACCESS_POINT,
+    device=DEVICE,
+    number_of_tests=NUMBER_OF_TESTS,
+    interval=INTERNVAL,
+    stressed=STRESSED_NETWORK,
+    room=ROOM,
+    notes=NOTES
 )
